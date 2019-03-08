16414e5b55c2127099293fe31dc1cb2308b74151
FujitaSatoru <fujitasatoru@FujitaSatorunoMacBook-Pro.local>
2019-03-08 20:05:43 +0900

    20190308 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :users
   get 'pages/index'
   root 'pages#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
 end
