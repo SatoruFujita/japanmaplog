7cbea8e100007d42d33335342337ce14fc964cb8
FujitaSatoru <fujitasatoru@FujitaSatorunoMacBook-Pro.local>
2019-03-15 13:52:02 +0900

    20190315 commit

---
+++ app/javascript/packs/components/hotelList.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'hotel-list',
+  props: ['vacant-hotel-data'],
+};
+</script>
+<template>
+  <div>
+    <ul id="test" class="list">
+      <li class="item" v-for="item of vacantHotelData" >
+
+      </li>
+    </ul>
+  </div>
+</template>
--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -4,10 +4,12 @@ import locale from 'element-ui/lib/locale/lang/ja'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from '../app.vue'
 import axios from 'axios'
+import HotelList from './components/hotelList.vue'
 
 Vue.use(ElementUI, { locale })
 Vue.config.productionTip = false
 
+document.addEventListener('DOMContentLoaded', () => {
 const app = new Vue({
   el: '#hotel-search',
   data:function(){
@@ -38,75 +40,26 @@ const app = new Vue({
   created: function(){
     this.search()
   },
-  //楽天トラベルAPI呼び出し
+  //楽天トラベルAPI呼び出しパラメータを分離して撮るようにする
   methods:{
-    search:function(){
+     search(){
         axios.get(`https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426
           ?applicationId=1011661123368946334&format=xml
-&checkinDate=2019-03-13
-&checkoutDate=2019-03-14
+&checkinDate=2019-03-15
+&checkoutDate=2019-03-16
 &latitude=128440.51
 &longitude=503172.21
-&searchRadius=1&elements=hotels`
+&searchRadius=1`
         )
         .then((response) => {
           console.log(response);
+          console.log(process.env.VUE_APP_GOOGLE_MAP_API_KEY);
           this.vacantHotelData = response.data.vacantHotelData;
         })
       }
-  }
+    },
+    components:{
+      HotelList
+    }
+  })
 })
-
-
-// The above code uses Vue without the compiler, which means you cannot
-// use Vue to target elements in your existing html templates. You would
-// need to always use single file components.
-// To be able to target elements in your existing html/erb templates,
-// comment out the above code and uncomment the below
-// Add <%= javascript_pack_tag 'hello_vue' %> to your layout
-// Then add this markup to your html template:
-//
-// <div id='hello'>
-//   {{message}}
-//   <app></app>
-// </div>
-
-
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
-//     },
-//     components: { App }
-//   })
-// })
-//
-//
-//
-// If the project is using turbolinks, install 'vue-turbolinks':
-//
-// yarn add vue-turbolinks
-//
-// Then uncomment the code block below:
-//
-// import TurbolinksAdapter from 'vue-turbolinks'
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// Vue.use(TurbolinksAdapter)
-//
-// document.addEventListener('turbolinks:load', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: () => {
-//       return {
-//         message: "Can you say hello?"
-//       }
-//     },
-//     components: { App }
-//   })
-// })
