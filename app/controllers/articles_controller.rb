57f5b4f165ef3178cdb41d5befdce7f6e735d33a
FujitaSatoru <fujitasatoru@FujitaSatorunoMacBook-Pro.local>
2019-03-24 21:52:02 +0900

    20190324-02 commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,23 +2,13 @@ class ArticlesController < ApplicationController
   protect_from_forgery except: :create
   #require 'exifr/jpeg'
   def create
-    #画像の情報を取得して都道府県名を取る
     imageFile = params['attachment1']
-    #@param_exif_lat = EXIFR::JPEG.new(@imageFile['filename']).gps.latitude
-    #@param_exif_lot = EXIFR::JPEG.new(@imageFile['filename']).gps.longitude
-
-    #Geocoder.configure(:language => :ja)
-    #@address_name = Geocoder.address(@param_exif_lat, @param_exif_lot)
-    #@prefecture_name  = @address_name.match(/.*[都道府県]/).to_s
-    #住所から都道府県名を抜き出し、PrefectureテーブルからIDをとる
-
-    prefectureId = @prefecureId
 
     @article = Article.new(
       title: params[:subject],
       body: params[:text],
       image: imageFile,
-      prefecture_id: prefectureId,
+      prefecture_id: @prefecureId,
       user_id: (params[:to].tr("@Japanmaplog.xyz", "").tr("post","")).to_i
     )
     if @article.save
